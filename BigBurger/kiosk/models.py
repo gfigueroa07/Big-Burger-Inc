9cbf05f02d0ad9dab8c41d343c9522191bcad905
gfigueroa07 <guillermofigueroa2840@gmail.com>
2025-08-07 20:47:32 -0400

    created models callewd customer, items & coupons.

--- BigBurger/kiosk/models.py
+++ BigBurger/kiosk/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from .category import Category
 
 class Customer(models.Model):
     id = models.UUIDField(auto_created=True, primary_key=True)
@@ -10,3 +9,24 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+class Items(models.Model):
+    id = models.UUIDField(auto_created=True, primary_key=True)
+    name = models.CharField(unique=True)
+    category = models.CharField(unique=True, max_length=20)
+    picture = models.ImageField(upload_to='item_images/', blank=True, null=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    
+class Coupons(models.Model):
+    id = models.UUIDField(auto_created=True, primary_key=True)
+    # items_id = models.UUIDField(auto_created=True, models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    name = models.CharField(unique=True)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    discount_perc = '' #wym by perc sensei? benefit? huh?
+    maximum_discount_value = models.DecimalField(max_digits=5, decimal_places=2)
+    max_units_per_customer = models.IntegerField(max_length=1)
+    single_use = models.BooleanField(default=True)
+    needed_item_quantity_to_use = models.IntegerField(default=1000000000)
+    applicable_on_item_amount = models.IntegerField(max_length=1)
+    
