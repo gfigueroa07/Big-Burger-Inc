ebf75d05e4b5e46d8780d5b0ac04df55a403c68c
gfigueroa07 <guillermofigueroa2840@gmail.com>
2025-08-09 19:23:34 -0400

    uhhh... kiosk app, models db and made migrations

--- BigBurger/kiosk/models.py
+++ BigBurger/kiosk/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+# Create your models here.
 class Customer(models.Model):
     id = models.UUIDField(auto_created=True, primary_key=True)
     name = models.CharField(max_length=100)
     email = models.CharField(unique=True, max_length=100)
-    telephone_number = models.DecimalField(max_digits=10)
-    verification_code = models.DecimalField(max_digits=4)
+    telephone_number = models.DecimalField(max_digits=10, decimal_places=2)
+    verification_code = models.DecimalField(max_digits=4,  decimal_places=2)
 
     def __str__(self):
         return self.name
@@ -18,15 +19,25 @@ class Items(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     is_available = models.BooleanField(default=True)
     
+    def __str__(self):
+        return self.name
+    
 class Coupons(models.Model):
     id = models.UUIDField(auto_created=True, primary_key=True)
-    items_id = models.ForeignKey(Items, auto_created=True)
+    items_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     name = models.CharField(unique=True)
     discount = models.DecimalField(max_digits=5, decimal_places=2)
-    discount_percentage = models.IntegerField(max_lenght=2) 
+    discount_percentage = models.IntegerField() 
     maximum_discount_value = models.DecimalField(max_digits=5, decimal_places=2)
-    max_units_per_customer = models.IntegerField(max_length=1)
+    max_units_per_customer = models.IntegerField()
     single_use = models.BooleanField(default=True)
     needed_item_quantity_to_use = models.IntegerField(default=1)
-    applicable_on_item_amount = models.IntegerField(max_length=1)
-    
+    applicable_on_item_amount = models.IntegerField()
+    id = models.UUIDField(auto_created=True, primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(unique=True, max_length=100)
+    telephone_number = models.DecimalField(max_digits=10, decimal_places=2)
+    verification_code = models.DecimalField(max_digits=4, decimal_places=2\)
+
+    def __str__(self):
+        return self.name
