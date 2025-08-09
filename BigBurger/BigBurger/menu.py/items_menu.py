2e4568904347f298ccea396d31343953d3429baf
gfigueroa07 <guillermofigueroa2840@gmail.com>
2025-08-09 12:12:54 -0400

    created menu, items_menu dwictonaries with item:price

---
+++ BigBurger/BigBurger/menu.py/items_menu.py
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+meal_menu = {
+    
+}
+
+kids_menu = {
+    
+}
+
+drinks = {
+    'Coca-Cola':1.99, 'Sprite':1.99, 'Mountain Dew':1.99, 'Dr. Pepper':1.99, 'Iced Tea':1.99, 'Pepsi':1.99, 'Strawberry Lemonade':2.99, 'Unsweetened Tea':2.49, 'Lemonade':2.99, 'Unsweetened Lemonade':1.99, 'Water Bottle':1.25
+ }
+
+sides = {
+    'Fries':2.49 , 'Hashbrown (1)':1.49 , 'Apple Pie (1)':1.99 , 'Chocolate Brownies (2)':3.99 , 'Mozzarella Sticks (5)':2.99
+}
+
+condiments = {
+    
+}
+
