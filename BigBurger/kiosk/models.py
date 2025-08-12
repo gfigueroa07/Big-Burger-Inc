35f8808f519a8053c5a50cc7022eb0c2715fc0c6
xarop-pa-toss <rpgdsantos@gmail.com>
2025-08-13 00:41:34 +0100

    Fixed model

--- BigBurger/kiosk/models.py
+++ BigBurger/kiosk/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
 
-class Items(models.Model):
+class Item(models.Model):
     id = models.UUIDField(auto_created=True, default=uuid.uuid4, primary_key=True)
     name = models.CharField(unique=True)
     category = models.CharField(unique=True, max_length=20)
@@ -9,11 +9,14 @@ class Items(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     is_available = models.BooleanField(default=True)
     
+    class Meta:
+        db_table = 'items'
+        
     def __str__(self):
         return self.name
     
 # Create your models here.
-class Customers(models.Model):
+class Customer(models.Model):
     id = models.UUIDField(auto_created=True, default=uuid.uuid4, primary_key=True)
     name = models.CharField(max_length=100)
     email = models.CharField(unique=True, max_length=100)
@@ -21,10 +24,13 @@ class Customers(models.Model):
     verification_code = models.DecimalField(max_digits=4,  decimal_places=2)
     coupons = models.ManyToManyField('Coupons', through='CustomerCoupon')
 
+    class Meta:
+        db_table = 'customers'
+
     def __str__(self):
         return self.name
     
-class Coupons(models.Model):
+class Coupon(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     items_id = models.ForeignKey(Items, on_delete=models.CASCADE)
     name = models.CharField(unique=True, max_length=100)
@@ -36,16 +42,21 @@ class Coupons(models.Model):
     needed_item_quantity_to_use = models.IntegerField(default=1)
     applicable_on_item_amount = models.IntegerField()
 
+    class Meta:
+        db_table = 'coupons'
+        
     def __str__(self):
         return self.name
+    
 
     
 class CustomerCoupon(models.Model):
-    customer = models.ForeignKey('Customers', on_delete=models.CASCADE)
-    coupon = models.ForeignKey('Coupons', on_delete=models.CASCADE)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    coupon = models.ForeignKey('Coupon', on_delete=models.CASCADE)
     acquired_at = models.DateTimeField()
     expires_at = models.DateTimeField()
     used_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         unique_together = ('customer', 'coupon')  # composite PK behavior
+        db_table = 'customer_coupons'
