2e37d19912869da312da2ea18d3453a15268b79d
xarop-pa-toss <rpgdsantos@gmail.com>
2025-08-12 23:51:18 +0100

    Finished base model with joint customer_coupons

--- BigBurger/kiosk/models.py
+++ BigBurger/kiosk/models.py
@@ -1,18 +1,8 @@
+import uuid
 from django.db import models
 
-# Create your models here.
-class Customer(models.Model):
-    id = models.UUIDField(auto_created=True, primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.CharField(unique=True, max_length=100)
-    telephone_number = models.DecimalField(max_digits=10, decimal_places=2)
-    verification_code = models.DecimalField(max_digits=4,  decimal_places=2)
-
-    def __str__(self):
-        return self.name
-
 class Items(models.Model):
-    id = models.UUIDField(auto_created=True, primary_key=True)
+    id = models.UUIDField(auto_created=True, default=uuid.uuid4, primary_key=True)
     name = models.CharField(unique=True)
     category = models.CharField(unique=True, max_length=20)
     picture = models.ImageField(upload_to='item_images/', blank=True, null=True)
@@ -22,22 +12,40 @@ class Items(models.Model):
     def __str__(self):
         return self.name
     
+# Create your models here.
+class Customer(models.Model):
+    id = models.UUIDField(auto_created=True, default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.CharField(unique=True, max_length=100)
+    telephone_number = models.DecimalField(max_digits=10, decimal_places=2)
+    verification_code = models.DecimalField(max_digits=4,  decimal_places=2)
+    coupons = models.ManyToManyField('Coupons', through='CustomerCoupon')
+
+    def __str__(self):
+        return self.name
+    
 class Coupons(models.Model):
-    id = models.UUIDField(auto_created=True, primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     items_id = models.ForeignKey(Items, on_delete=models.CASCADE)
-    name = models.CharField(unique=True)
+    name = models.CharField(unique=True, max_length=100)
     discount = models.DecimalField(max_digits=5, decimal_places=2)
-    discount_percentage = models.IntegerField() 
+    discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
     maximum_discount_value = models.DecimalField(max_digits=5, decimal_places=2)
     max_units_per_customer = models.IntegerField()
     single_use = models.BooleanField(default=True)
     needed_item_quantity_to_use = models.IntegerField(default=1)
     applicable_on_item_amount = models.IntegerField()
-    id = models.UUIDField(auto_created=True, primary_key=True)
-    name = models.CharField(max_length=100)
-    email = models.CharField(unique=True, max_length=100)
-    telephone_number = models.DecimalField(max_digits=10, decimal_places=2)
-    verification_code = models.DecimalField(max_digits=4, decimal_places=2)
 
     def __str__(self):
         return self.name
+
+    
+class CustomerCoupon(models.Model):
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    coupon = models.ForeignKey('Coupons', on_delete=models.CASCADE)
+    acquired_at = models.DateTimeField()
+    expires_at = models.DateTimeField()
+    used_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('customer', 'coupon')  # composite PK behavior
